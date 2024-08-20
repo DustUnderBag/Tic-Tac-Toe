7a4d7b23e65850e60d477ce912686df10029d60a
DustUnderBag <usamary55@gmail.com>
2024-08-20 00:49:41 -0400

    Create console version of game with basic features
    Made components:
    - Gameboard with methods: getBoard, printBoard, drawCell.
    - Cell with methods: getMarker, markerCell.
    - Player objects with name and its unqie marker.

---
+++ script.js
@@ -0,0 +1,72 @@
+/*
+Gameboard represents the state of the gameboard.
+Responsibilities: 
+    - Returns the whole board.
+    - Print board that shows cell values to the console.
+    - Draw marker on cells, takes player and pos as parameters.
+*/
+const Gameboard = (function () {
+    const rows = 3;
+    const board = [];   
+
+    for(let i = 0; i < 9; i++) {
+        board.push(Cell());
+    }
+
+    const getBoard = () => board;
+
+    const printBoard = function() {
+        const boardWithCellValue = board.map(cell => cell.getMarker());
+        console.log(boardWithCellValue[0],boardWithCellValue[1],boardWithCellValue[2]);
+        console.log(boardWithCellValue[3],boardWithCellValue[4],boardWithCellValue[5]);
+        console.log(boardWithCellValue[6],boardWithCellValue[7],boardWithCellValue[8]);
+    }
+    
+    const drawCell = function(player, pos) {
+        board[pos].markCell(player);
+        printBoard();
+    }
+
+
+    return {
+        getBoard,
+        printBoard,
+        drawCell,
+    }
+})();
+
+
+//A function factory for Cell object that contains two methods.
+/*
+Responsibilities:
+    - Returns target cell marker value.
+    - Draw marker on target cell.
+*/
+function Cell() {
+    let marker = 0;
+
+    const getMarker = () => marker;
+
+    const markCell = function(player) {
+        marker = player.marker;
+    }
+
+    return {
+        getMarker,
+        markCell,
+    }
+}
+
+/*
+    Player objects, that stores player name and marker
+*/
+const player = function(marker) {
+    const playerName = "Player " + marker;
+    return {playerName, marker};
+}
+
+const playerX = player("X");
+const playerO = player("O");
+
+Gameboard.printBoard(); 
+
