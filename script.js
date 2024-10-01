cb08bc687a9cca01e16b08167024b69c1d29333a
DustUnderBag <usamary55@gmail.com>
2024-10-01 19:30:53 -0400

    Hide robot against robot window when game starts

--- script.js
+++ script.js
@@ -831,6 +831,7 @@ function bot(marker, Gameboard, difficulty) {
 
     function startGame_twoBots() {
         mode = 2;
+        robotTwoWindow.style.display = "none";
         gameDiv.style.display = "flex";
 
         difficultyX = document.querySelector('input[name="difficultyX"]:checked').value;
